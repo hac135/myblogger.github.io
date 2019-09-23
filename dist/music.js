37e27f4cd87b7cd86516bec743f5921a9712f7ea
hac135 <1406654070@qq.com>
2019-09-23 22:05:34 +0800

    Site updated: 2019-09-23 22:05:31

--- dist/music.js
+++ dist/music.js
@@ -4,6 +4,12 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
 	  {
+        name: 'dear,thats not love',
+        artist: 'LiFanyi',
+        url: 'http://freetyst.nf.migu.cn/public/product4th/product36/2019/09/0620/2019%E5%B9%B409%E6%9C%8806%E6%97%A519%E7%82%B934%E5%88%86%E5%86%85%E5%AE%B9%E5%87%86%E5%85%A5%E5%92%AA%E5%92%95%E9%9F%B3%E4%B9%90%E8%87%AA%E6%9C%89%E7%89%88%E6%9D%834222%E9%A6%96/%E6%A0%87%E6%B8%85%E9%AB%98%E6%B8%85/MP3_320_16_Stero/6990539Z2BA.mp3?channelid=03&k=0909d8bfda1e2102&t=1569254540&msisdn=89b43b31-92b4-4fe6-807b-2eaefd63e15b',
+        cover: 'https://github.com/hac135/BlogPhotos/blob/master/Images/001.jpg?raw=true',
+      },
+	  {
         name: '说好不哭',
         artist: 'Jay',
         url: 'http://freetyst.nf.migu.cn/public/product4th/product36/2019/09/1622/2019%E5%B9%B409%E6%9C%8816%E6%97%A522%E7%82%B944%E5%88%86%E7%B4%A7%E6%80%A5%E5%86%85%E5%AE%B9%E5%87%86%E5%85%A5%E7%BA%B5%E6%A8%AA%E4%B8%96%E4%BB%A31%E9%A6%96/%E6%A0%87%E6%B8%85%E9%AB%98%E6%B8%85/MP3_320_16_Stero/6005479Z068.mp3?channelid=03&k=3c11d369a3900919&t=1568735056&msisdn=8453ad6c-a3ad-42ee-a8a5-6cd764b62d05',
