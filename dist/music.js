f439e808a88a355df6577282df0625f9679f6dd1
hac135 <1406654070@qq.com>
2021-07-01 19:27:17 +0800

    Site updated: 2021-07-01 19:27:06

---
+++ dist/music.js
@@ -0,0 +1,82 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	  {
+        name: 'dear,thats not love',
+        artist: 'LiFanyi',
+        url: 'http://freetyst.nf.migu.cn/public/product4th/product36/2019/09/0620/2019%E5%B9%B409%E6%9C%8806%E6%97%A519%E7%82%B934%E5%88%86%E5%86%85%E5%AE%B9%E5%87%86%E5%85%A5%E5%92%AA%E5%92%95%E9%9F%B3%E4%B9%90%E8%87%AA%E6%9C%89%E7%89%88%E6%9D%834222%E9%A6%96/%E6%A0%87%E6%B8%85%E9%AB%98%E6%B8%85/MP3_320_16_Stero/6990539Z2BA.mp3?channelid=03&k=0909d8bfda1e2102&t=1569254540&msisdn=89b43b31-92b4-4fe6-807b-2eaefd63e15b',
+        cover: 'https://www.vxotu.com/u/20200315/19190110.jpg',
+      },
+	  {
+        name: '说好不哭',
+        artist: 'Jay',
+        url: 'http://freetyst.nf.migu.cn/public/product4th/product36/2019/09/1622/2019%E5%B9%B409%E6%9C%8816%E6%97%A522%E7%82%B944%E5%88%86%E7%B4%A7%E6%80%A5%E5%86%85%E5%AE%B9%E5%87%86%E5%85%A5%E7%BA%B5%E6%A8%AA%E4%B8%96%E4%BB%A31%E9%A6%96/%E6%A0%87%E6%B8%85%E9%AB%98%E6%B8%85/MP3_320_16_Stero/6005479Z068.mp3?channelid=03&k=3c11d369a3900919&t=1568735056&msisdn=8453ad6c-a3ad-42ee-a8a5-6cd764b62d05',
+        cover: 'https://www.vxotu.com/u/20200315/19190110.jpg',
+      },
+	  {
+        name: 'meet',
+        artist: 'SunYanzi',
+        url: 'http://rn01.sycdn.kuwo.cn/9255b078fe5a86e76a19e3e75c198ce4/5e709b1e/resource/n3/20/58/242205905.mp3',
+        cover: 'https://www.vxotu.com/u/20200315/19190110.jpg',
+      },
+	
+	  
+	
+	  {
+        name: 'someone like me',
+        artist: 'MaoBuyi',
+        url: 'http://mp.111ttt.cn/mp3music/40471965.mp3',
+        cover: 'https://github.com/hac135/my_images/blob/master/self.jpg?raw=true',
+      },
+	  {
+        name: 'anhe brige',
+        artist: 'yuxi',
+        url: 'http://www.ytmp3.cn/down/58164.mp3',
+        cover: 'https://www.vxotu.com/u/20200315/19190110.jpg',
+      },
+	  {
+        name: 'snail',
+        artist: 'jay',
+        url: 'http://www.ytmp3.cn/down/38970.mp3',
+        cover: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555674532&di=c5bc1ade78a314aca5006168f4aae7ef&imgtype=jpg&er=1&src=http%3A%2F%2Fimg5.duitang.com%2Fuploads%2Fitem%2F201410%2F09%2F20141009171137_mdvXy.thumb.700_0.png',
+	  },
+	  {
+        name: 'poems wrote by my father',
+        artist: 'xufei',
+        url: 'http://www.ytmp3.cn/down/48256.mp3',
+        cover: 'https://github.com/hac135/my_images/blob/master/sunset.jpg?raw=true',
+      },
+	  {
+        name: 'chendu',
+        artist: 'zhaolei',
+        url: 'http://www.ytmp3.cn/down/38582.mp3',
+        cover: 'https://github.com/hac135/BlogPhotos/blob/master/Images/001.jpg?raw=true',
+      },
+	  {
+        name: 'if love is destined',
+        artist: 'Lijian',
+        url: 'http://www.ytmp3.cn/down/33338.mp3',
+        cover: 'https://github.com/hac135/BlogPhotos/blob/master/Images/001.jpg?raw=true',
+      },
+	  {
+        name: 'poems wrote by my father',
+        artist: 'Lijian',
+        url: 'http://www.ytmp3.cn/down/43363.mp3',
+        cover: 'https://github.com/hac135/BlogPhotos/blob/master/Images/001.jpg?raw=true',
+      },
+	  {
+        name: 'see you again',
+        artist: 'lmg',
+        url: 'http://www.ytmp3.cn/down/51245.mp3',
+        cover: 'https://github.com/hac135/BlogPhotos/blob/master/Images/001.jpg?raw=true',
+      },
+	  {
+        name: 'Viva La Vida',
+        artist: 'coldplay',
+        url: 'http://www.ytmp3.cn/down/49739.mp3',
+        cover: 'https://github.com/hac135/BlogPhotos/blob/master/Images/001.jpg?raw=true',
+      }
+    ]
+});
